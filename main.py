8e6d8b8e9302216fd02f64b5686703824a26c1b3
Nic <73386479+nicyoong@users.noreply.github.com>
2025-11-06 17:07:39 +0800

    globals

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ import discord
 import yaml
 from dotenv import load_dotenv
 from discord import commands, tasks
+
+
+load_dotenv()
+typing_lock = asyncio.Lock()
+urgent_queue = []
+URGENT_TRIGGER = botutils.URGENT_TRIGGER
+URGENT_TRIGGER1H = botutils.URGENT_TRIGGER1H
+URGENT_RESPONSES = [
+    "I’m here, tell me what’s going on.",
+    "Hey, I’m here, what happened?",
+    "I’m here now. What’s going on?",
+    "I’m listening. What’s wrong?",
+    "Hey, I’m here. You okay?",
+]
